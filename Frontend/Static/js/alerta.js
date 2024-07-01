cd7401b37f6f62b764a2d83c119f200dae057264
mario4796 <mario.checchia@outlook.com>
2024-07-01 19:24:06 -0300

    Se quito la carpeta frontend para cambio de repositorio

--- Frontend/Static/js/alerta.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener(onload,
-    alert("Esto es solo una página de muestra, al presionar en comprar no lleva a ningún lado.")
-    
-)
-
-document.getElementById('buy-btn').onclick = function(){
-    alert("Esto es solo una página de muestra, al presionar en comprar no lleva a ningún lado.");
-}
